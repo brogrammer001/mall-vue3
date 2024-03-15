b1903a4a26ba365fd6124eb1dd1b8b3f4de3071f
bathfire <>
2024-03-15 16:51:57 +0800

    测试

--- src/components/search/SearchIndex.vue
+++ src/components/search/SearchIndex.vue
@@ -1,11 +1,11 @@
 <template>
-  
+    <IndexHeaderSelect />
+    <IndexHeadSerch />
 </template>
 
 <script setup>
-
+import IndexHeaderSelect from './IndexHeaderSelect.vue'
+import IndexHeadSerch from './IndexHeadSerch.vue'
 </script>
 
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
